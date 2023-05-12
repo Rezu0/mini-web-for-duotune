a718fa6cac34e771b35dd88d84f3bb4fcfd54541
Rezu <54113198+Rezu0@users.noreply.github.com>
2023-05-12 15:24:58 +0700

    first commit upload file

---
+++ js/index.js
@@ -0,0 +1,38 @@
+const express = require('express')
+const app = express()
+const port = 5500
+const fs = require('fs');
+const cors = require('cors')
+
+app.use(cors())
+
+app.get('/count/:dir', (req, res) => {
+    const dir = req.params.dir
+    const mainPath = `../duotune/${dir}`
+
+    const countFiles = (dirPath) => {
+        fs.readdir(dirPath, (err, files) => {
+            if(err){
+                console.error(`Terjadi Kesalahan: ${err}`);
+                res.json({
+                    dir: dir,
+                    count: 0
+                })
+                return;
+            }
+    
+            const fileCount = files.length
+            res.json({
+                dir: dir,
+                count: fileCount
+            })
+        })
+    }
+
+    countFiles(mainPath)
+
+});
+
+app.listen(port, () => {
+    console.log(`Server berjalan di http://localhost:${port}`);
+})
